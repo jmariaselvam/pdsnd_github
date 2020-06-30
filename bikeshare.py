e4669f6d0b2b258fa86e0b42f8d8790cbbf751a4
j.mariaselvam <j.mariaselvam@groupeonepoint.com>
2020-06-30 16:35:14 +0200

    docs: correct get_filters function doc

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@
 
     def get_filters():
         """
-        Asks user to specify a city, month, and day to analyze.
+        Asks user to specify a city, month and day to analyze.
 
         Returns:
             (str) city - name of the city to analyze
